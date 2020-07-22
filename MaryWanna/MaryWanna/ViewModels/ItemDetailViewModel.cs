bb91c5a28945a22c0ebe93ebe4f8105f6961ab86
Jeremy-Dane Ferguson <jeremy_df9@hotmail.com>
2020-07-22 08:02:39 -0500

    Add project files.

---
+++ MaryWanna/MaryWanna/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MaryWanna.Models;
+
+namespace MaryWanna.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
